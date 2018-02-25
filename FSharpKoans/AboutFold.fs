0ca0ce2cd4ba0ce2b087780734b1048cadd3ceef
CorbynL <corbynlangley@gmail.com>
2018-02-25 21:26:43 +0200

    Completed AboutFold

--- FSharpKoans/AboutFold.fs
+++ FSharpKoans/AboutFold.fs
@@ -34,19 +34,27 @@ or something else), it's likely that you'll be able to use a fold.
 module ``17: Welcome to the functional fold`` =
     [<Test>]
     let ``01 A fold which sums a list`` () =
-        let fold initialState xs =
-            __ // write a function to do what's described above
+        let fold initialState xs =      // write a function to do what's described above
+            let rec folder i list =
+                match list with
+                | [] -> i
+                | head::tail -> folder (i + head) tail
+            folder initialState xs 
         fold 0 [1; 2; 3; 4] |> should equal 10
         fold 100 [2;4;6;8] |> should equal 120
 
     [<Test>]
     let ``02 A fold which multiplies a list`` () =
-        let fold initialState xs =
-            __ // write a function to multiply the elements of a list
-        fold __ [99] |> should equal 99
-        fold 2 [__] |> should equal 22
-        fold __ [1;3;5;7] |> should equal 105
-        fold __ [2;5;3] |> should equal 0
+        let fold initialState xs =      // write a function to multiply the elements of a list
+            let rec folder i list =
+                match list with
+                | [] -> i
+                | head::tail -> folder (i*head) tail
+            folder initialState xs 
+        fold 1 [99] |> should equal 99
+        fold 2 [11] |> should equal 22
+        fold 1 [1;3;5;7] |> should equal 105
+        fold 0 [2;5;3] |> should equal 0
 
     // you probably know the drill by now.  It'd be good to have
     // a function which does the state-generation stuff, wouldn't
@@ -55,7 +63,11 @@ module ``17: Welcome to the functional fold`` =
     [<Test>]
     let ``03 Folding, the hard way`` () =
         let fold (f : 'a -> 'b -> 'a) (initialState : 'a) (xs : 'b list) : 'a =
-            __  // write a function to do a fold.
+            let rec folder i list =             // write a function to do a fold.
+                match list with
+                | [] -> i
+                | head::tail -> folder (f i head) tail
+            folder initialState xs
         fold (+) 0 [1;2;3;4] |> should equal 10
         fold (*) 2 [1;2;3;4] |> should equal 48
         fold (fun state item -> sprintf "%s %s" state item) "items:" ["dog"; "cat"; "bat"; "rat"]
